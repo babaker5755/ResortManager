817732db0362e826020ee216542c5bc3062d5a73
Cris M <cemendoza0611@eagle.fgcu.edu>
2019-10-05 22:48:50 -0400

    Changed string on line 15 to "Bottom Text"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Testing");
+        primaryStage.setTitle("Bottom Text");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
