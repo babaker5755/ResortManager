1fc1d72b57036c3a7a87d617d79e18468ca53884
Brandon <bbrandonbaker.b@gmail.com>
2019-10-15 16:30:57 -0400

    Created Classes

---
+++ src/sample/Booking.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Booking {
+
+  private String clientName;
+  private String clientAddress;
+  private String clientCreditCard;
+  private String clientEmail;
+  private String roomNumber;
+  private String confirmationNumber;
+  private double price;
+
+}
