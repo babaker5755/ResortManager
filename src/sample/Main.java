d78ecd5772d04f676874b7691e8f1351e1f95165
Brandon <bbrandonbaker.b@gmail.com>
2019-10-16 21:25:16 -0400

    catalog tab pane started

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,15 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-// This is an edit so I can do a pull request. -Todd
-//this is an edit for pull request.  -Jerry..
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Testing");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
     }
 
--- src/sample/Room.java
+++ src/sample/Room.java
@@ -10,4 +10,52 @@ public class Room {
   private Boolean isVacant;
   private double price;
 
+  public Room(String roomNumber, String bedSize, int numBeds, Boolean isVacant, double price) {
+    this.roomNumber = roomNumber;
+    this.bedSize = bedSize;
+    this.numBeds = numBeds;
+    this.isVacant = isVacant;
+    this.price = price;
+  }
+
+  public String getRoomNumber() {
+    return roomNumber;
+  }
+
+  public void setRoomNumber(String roomNumber) {
+    this.roomNumber = roomNumber;
+  }
+
+  public String getBedSize() {
+    return bedSize;
+  }
+
+  public void setBedSize(String bedSize) {
+    this.bedSize = bedSize;
+  }
+
+  public int getNumBeds() {
+    return numBeds;
+  }
+
+  public void setNumBeds(int numBeds) {
+    this.numBeds = numBeds;
+  }
+
+  public Boolean getVacant() {
+    return isVacant;
+  }
+
+  public void setVacant(Boolean vacant) {
+    isVacant = vacant;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
 }
