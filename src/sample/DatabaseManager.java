4c734b1f14faf6ab621c9210ec777fb3e94650a7
Brandon <bbrandonbaker.b@gmail.com>
2019-10-21 22:25:52 -0400

    auto-formatted all files

--- src/sample/DatabaseManager.java
+++ src/sample/DatabaseManager.java
@@ -5,157 +5,204 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class DatabaseManager {
-    private Connection conn = null;
-    private PreparedStatement ps = null;
-    private ResultSet rs = null;
-    private boolean isConnectedToDB;
+  private Connection conn = null;
+  private PreparedStatement ps = null;
+  private ResultSet rs = null;
+  private boolean isConnectedToDB;
 
-    boolean getConnectionStatus() {return isConnectedToDB;}
+  boolean getConnectionStatus() {
+    return isConnectedToDB;
+  }
 
-    DatabaseManager() {
-        connectToDB();
+  DatabaseManager() {
+    connectToDB();
+  }
+
+  private void connectToDB() {
+
+    try {
+      // Establish a connection to the database and flag the DBManager as connected.
+      Class.forName("org.h2.Driver");
+      conn = DriverManager.getConnection("jdbc:h2:./res/Hotel_Database");
+      isConnectedToDB = true;
+
+      // Cover cases where a connection could not be made.
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
     }
+  }
 
-    private void connectToDB() {
+  void addRoomsToDB(ArrayList<Room> rooms) {
 
+    for (Room room : rooms) {
+      System.out.println("Inserting room records into table...");
+      try {
+        PreparedStatement ps =
+            conn.prepareStatement(
+                "INSERT INTO ROOMS VALUES ('"
+                    + room.getRoomNumber()
+                    + "', '"
+                    + room.getBedSize()
+                    + "', "
+                    + room.getNumBeds()
+                    + ", "
+                    + room.getVacant()
+                    + ", "
+                    + room.getPrice()
+                    + ")");
+        ps.executeUpdate();
+        System.out.println("Inserted room record into table.");
+      } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println("Could not create room record.");
+      }
+    }
+  }
+  // add bookings to database
+  void addBookingsToDB(ArrayList<Booking> bookingList) {
+    System.out.println("Inserting booking records into table...");
+    for (Booking booking : bookingList) {
+      try {
+        PreparedStatement ps =
+            conn.prepareStatement(
+                "INSERT INTO BOOKINGS VALUES ('"
+                    + booking.getConfirmationNumber()
+                    + "', '"
+                    + booking.getRoomNumber()
+                    + "', '"
+                    + booking.getPrice()
+                    + "', '"
+                    + booking.getClientName()
+                    + "', '"
+                    + booking.getClientAddress()
+                    + "', '"
+                    + booking.getClientCreditCard()
+                    + "', '"
+                    + booking.getClientEmail()
+                    + "', '"
+                    + booking.getCheckInDate()
+                    + "', '"
+                    + booking.getCheckOutDate()
+                    + "')");
+        ps.executeUpdate();
+        System.out.println("Inserted booking record into table.");
+      } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println("Could not create booking record.");
+      }
+    }
+  }
+  // delete booking from database and ArrayList
+  void removeBookingsFromDB(ArrayList<Booking> bookingList, String confirmationNumber) {
+    System.out.println("Canceling booking...");
+    for (Booking booking : bookingList) {
+      if (booking.getConfirmationNumber().equals(confirmationNumber)) {
         try {
-            // Establish a connection to the database and flag the DBManager as connected.
-            Class.forName("org.h2.Driver");
-            conn = DriverManager.getConnection("jdbc:h2:./res/Hotel_Database");
-            isConnectedToDB = true;
-
-            // Cover cases where a connection could not be made.
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    void addRoomsToDB(ArrayList<Room> rooms) {
-
-        for (Room room : rooms) {
-            System.out.println("Inserting room records into table...");
-            try {
-                PreparedStatement ps = conn.prepareStatement("INSERT INTO ROOMS VALUES ('" + room.getRoomNumber() + "', '" +
-                        room.getBedSize() + "', " + room.getNumBeds() + ", " +
-                        room.getVacant() + ", " + room.getPrice() + ")");
-                ps.executeUpdate();
-                System.out.println("Inserted room record into table.");
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.out.println("Could not create room record.");
-            }
-        }
-    }
-    // add bookings to database
-    void addBookingsToDB(ArrayList<Booking> bookingList){
-        System.out.println("Inserting booking records into table...");
-        for (Booking booking : bookingList) {
-            try {
-                PreparedStatement ps = conn.prepareStatement("INSERT INTO BOOKINGS VALUES ('" + booking.getConfirmationNumber() + "', '" +
-                        booking.getRoomNumber() + "', '" + booking.getPrice() + "', '" +
-                        booking.getClientName() + "', '" + booking.getClientAddress() + "', '" +
-                        booking.getClientCreditCard() + "', '" + booking.getClientEmail() + "', '" +
-                        booking.getCheckInDate() + "', '" + booking.getCheckOutDate() + "')");
-                ps.executeUpdate();
-                System.out.println("Inserted booking record into table.");
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.out.println("Could not create booking record.");
-            }
-        }
-    }
-    // delete booking from database and ArrayList
-    void removeBookingsFromDB(ArrayList<Booking> bookingList, String confirmationNumber){
-        System.out.println("Canceling booking...");
-        for (Booking booking : bookingList){
-            if(booking.getConfirmationNumber().equals(confirmationNumber)){
-                try {
-                    bookingList.remove(booking);
-                    PreparedStatement ps = conn.prepareStatement("DELETE FROM BOOKINGS WHERE CONFIRMATION_NUMBER='" + booking.getConfirmationNumber() + "'");
-                    ps.executeUpdate();
-                    System.out.println("Canceled booking.");
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                    System.out.println("Could not cancel booking.");
-                }
-
-            }
-        }
-    }
-
-    ArrayList<Room> getRoomsAsList() {
-        ArrayList<Room> rooms = new ArrayList<>();
-
-        try {
-            // Get all rows from the specified table.
-            ps = conn.prepareStatement("SELECT * FROM ROOMS");
-            rs = ps.executeQuery();
-
-            // Make each row a room object, then add it to the list of rooms.
-            while (rs.next()) {
-                Room room = new Room(rs.getString("ROOM_NUMBER"),
-                        rs.getString("BED_SIZE"),
-                        rs.getInt("NUM_BEDS"),
-                        rs.getBoolean("IS_VACANT"),
-                        rs.getDouble("PRICE"));
-                rooms.add(room);
-
-                // Cumbersome console confirmation.
-                System.out.println(
-                        "ROOM_NUMBER: " + rs.getString("ROOM_NUMBER") +
-                        ", BED_SIZE: " + rs.getString("BED_SIZE") +
-                        ", NUM_BEDS: " + rs.getInt("NUM_BEDS") +
-                        ", IS_VACANT: " + rs.getBoolean("IS_VACANT") +
-                        ", PRICE: " + rs.getDouble("PRICE"));
-            }
+          bookingList.remove(booking);
+          PreparedStatement ps =
+              conn.prepareStatement(
+                  "DELETE FROM BOOKINGS WHERE CONFIRMATION_NUMBER='"
+                      + booking.getConfirmationNumber()
+                      + "'");
+          ps.executeUpdate();
+          System.out.println("Canceled booking.");
         } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.print("Could not execute query.");
+          e.printStackTrace();
+          System.out.println("Could not cancel booking.");
         }
+      }
+    }
+  }
 
-        return rooms;
+  ArrayList<Room> getRoomsAsList() {
+    ArrayList<Room> rooms = new ArrayList<>();
+
+    try {
+      // Get all rows from the specified table.
+      ps = conn.prepareStatement("SELECT * FROM ROOMS");
+      rs = ps.executeQuery();
+
+      // Make each row a room object, then add it to the list of rooms.
+      while (rs.next()) {
+        Room room =
+            new Room(
+                rs.getString("ROOM_NUMBER"),
+                rs.getString("BED_SIZE"),
+                rs.getInt("NUM_BEDS"),
+                rs.getBoolean("IS_VACANT"),
+                rs.getDouble("PRICE"));
+        rooms.add(room);
+
+        // Cumbersome console confirmation.
+        System.out.println(
+            "ROOM_NUMBER: "
+                + rs.getString("ROOM_NUMBER")
+                + ", BED_SIZE: "
+                + rs.getString("BED_SIZE")
+                + ", NUM_BEDS: "
+                + rs.getInt("NUM_BEDS")
+                + ", IS_VACANT: "
+                + rs.getBoolean("IS_VACANT")
+                + ", PRICE: "
+                + rs.getDouble("PRICE"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.print("Could not execute query.");
     }
 
-    ArrayList<Booking> getBookingsAsList() {
-        ArrayList<Booking> bookingList = new ArrayList<>();
+    return rooms;
+  }
 
-        try {
-            // Get all rows from the specified table.
-            ps = conn.prepareStatement("SELECT * FROM BOOKINGS");
-            rs = ps.executeQuery();
+  ArrayList<Booking> getBookingsAsList() {
+    ArrayList<Booking> bookingList = new ArrayList<>();
 
-            // Make each row a Booking object, then add it to the list of bookings.
-            while (rs.next()) {
-                Booking booking = new Booking(rs.getString("CONFIRMATION_NUMBER"),
-                        rs.getString("ROOM_NUMBER"),
-                        rs.getDouble("PRICE"),
-                        rs.getString("CLIENT_NAME"),
-                        rs.getString("CLIENT_ADDRESS"),
-                        rs.getString("CLIENT_CREDITCARD"),
-                        rs.getString("CLIENT_EMAIL"),
-                        rs.getDate("START_DATE"),
-                        rs.getDate("END_DATE"));
-                bookingList.add(booking);
+    try {
+      // Get all rows from the specified table.
+      ps = conn.prepareStatement("SELECT * FROM BOOKINGS");
+      rs = ps.executeQuery();
 
-                // Confirm in console.
-                System.out.println(
-                        "CONFIRMATION_NUMBER: " + rs.getString("CONFIRMATION_NUMBER") +
-                        ", ROOM_NUMBER: " + rs.getString("ROOM_NUMBER") +
-                        ", PRICE: " + rs.getDouble("PRICE") +
-                        ", CLIENT_NAME: " + rs.getString("CLIENT_NAME") +
-                        ", CLIENT_ADDRESS: " + rs.getString("CLIENT_ADDRESS") +
-                        ", CLIENT_CREDITCARD: " + rs.getString("CLIENT_CREDITCARD") +
-                        ", CLIENT_EMAIL: " + rs.getString("CLIENT_EMAIL") +
-                        ", START_DATE: " + rs.getDate("START_DATE") +
-                        ", END_DATE: " + rs.getDate("END_DATE"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.print("Could not execute query.");
-        }
+      // Make each row a Booking object, then add it to the list of bookings.
+      while (rs.next()) {
+        Booking booking =
+            new Booking(
+                rs.getString("CONFIRMATION_NUMBER"),
+                rs.getString("ROOM_NUMBER"),
+                rs.getDouble("PRICE"),
+                rs.getString("CLIENT_NAME"),
+                rs.getString("CLIENT_ADDRESS"),
+                rs.getString("CLIENT_CREDITCARD"),
+                rs.getString("CLIENT_EMAIL"),
+                rs.getDate("START_DATE"),
+                rs.getDate("END_DATE"));
+        bookingList.add(booking);
 
-        return bookingList;
+        // Confirm in console.
+        System.out.println(
+            "CONFIRMATION_NUMBER: "
+                + rs.getString("CONFIRMATION_NUMBER")
+                + ", ROOM_NUMBER: "
+                + rs.getString("ROOM_NUMBER")
+                + ", PRICE: "
+                + rs.getDouble("PRICE")
+                + ", CLIENT_NAME: "
+                + rs.getString("CLIENT_NAME")
+                + ", CLIENT_ADDRESS: "
+                + rs.getString("CLIENT_ADDRESS")
+                + ", CLIENT_CREDITCARD: "
+                + rs.getString("CLIENT_CREDITCARD")
+                + ", CLIENT_EMAIL: "
+                + rs.getString("CLIENT_EMAIL")
+                + ", START_DATE: "
+                + rs.getDate("START_DATE")
+                + ", END_DATE: "
+                + rs.getDate("END_DATE"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.print("Could not execute query.");
     }
 
-
+    return bookingList;
+  }
 }
